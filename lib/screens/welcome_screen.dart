8940995a0d32119d3a1424a7e4e9a634b3beb4c7
Harsh <harshmacc@gmail.com>
2020-01-12 07:24:36 +0530

    added a curved animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,14 +3,46 @@ import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
-
   static const String id = 'welcome_screen';
 
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+
+    );
+
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+
+    controller.forward();
+
+    animation.addStatusListener((status){
+      if (status == AnimationStatus.completed){
+        controller.reverse(from: 1);
+      }
+      else if (status == AnimationStatus.dismissed){
+        controller.forward();
+      }
+    });
+
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +59,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: animation.value * 60.0,
                   ),
                 ),
                 Text(
